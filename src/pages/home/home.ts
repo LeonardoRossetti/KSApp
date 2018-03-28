37b1d41d9cf1f6853955401c5fa7311ae7bbcfa9
Leonardo Rossetti <leoo.rossetti@gmail.com>
2018-03-27 23:30:57 -0300

    Adjusting pages and make a login method works

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 
+import { AuthService } from '../../providers/auth/auth.service';
 import { SignupPage } from '../signup/signup';
 import { User } from '../../models/user.models';
 import { UserService } from '../../providers/user/user.service';
@@ -14,21 +15,27 @@ export class HomePage {
 
   users: Observable<User[]>;
 
-  constructor(public navCtrl: NavController, 
+  constructor(
+              public authService: AuthService,
+              public navCtrl: NavController, 
               public navParams: NavParams,
               public userService: UserService) {
+  }
+
+  ionViewCanEnter(): Promise<boolean> {
+    return this.authService.authenticated;
   }
 
   ionViewDidLoad(){
     this.users = this.userService.getAll();
   }
 
-  onSignup(): void{
-    this.navCtrl.push(SignupPage)
-  }
-
   onChatCreate(user): void{
     console.log(user);
+  }
+
+  onLogout(): void{
+    this.authService.logout();
   }
 
 
--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -72,5 +72,4 @@ export class SigninPage {
       buttons: ['Ok']
     }).present();
   }
-
 }
